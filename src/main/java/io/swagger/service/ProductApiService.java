19bfbd9a28ad8b610664bbe6deab6e07c3d13c27
D0V4H-K11N <126996606+D0V4H-K11N@users.noreply.github.com>
2024-07-18 13:18:04 +0200

    Update ProductApiService.java

--- src/main/java/io/swagger/service/ProductApiService.java
+++ src/main/java/io/swagger/service/ProductApiService.java
@@ -18,7 +18,7 @@ public class ProductApiService {
     }
 
     public List<Document> findName(String name) {
-        Query query = new Query(Criteria.where("name").is(name);
+        Query query = new Query(Criteria.where("name").is(name));
         if(mongoTemplate.find(query, Document.class, "products") == null) {
             throw new RuntimeException("Product does not exist");
         }
